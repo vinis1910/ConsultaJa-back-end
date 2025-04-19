66299d1e5c8004efb831e886763fee402bc4cdaa
vgbezerra <vgbezerra@viptech.com.br>
2025-04-18 21:44:39 -0300

    init project

---
+++ src/app.modules.ts
@@ -0,0 +1,37 @@
+import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+
+import { AuthModule } from './auth/auth.module';
+import { UsersModule } from './users/users.module';
+import { PatientsModule } from './patients/patients.module';
+import { DoctorsModule } from './doctors/doctors.module';
+import { AppointmentsModule } from './appointments/appointments.module';
+import { ChatsModule } from './chats/chats.module';
+
+@Module({
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: process.env.DB_HOST,
+      port: Number(process.env.DB_PORT) || 5432,
+      username: process.env.DB_USER,
+      password: process.env.DB_PASS,
+      database: process.env.DB_NAME,
+      autoLoadEntities: true,
+      synchronize: false,
+    }),
+
+    AuthModule,
+    UsersModule,
+    PatientsModule,
+    DoctorsModule,
+    AppointmentsModule,
+    ChatsModule,
+  ],
+})
+export class AppModule {}
