42b653275473aa9b7f9aeaf60da2d7046caa0683
vgbezerra <vgbezerra@viptech.com.br>
2025-06-15 22:56:29 -0300

    feat: upload documents

--- src/attachments/AttachmentsController.ts
+++ src/attachments/AttachmentsController.ts
@@ -1,4 +1,24 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, HttpException, HttpStatus, InternalServerErrorException, Post, UploadedFile, UseInterceptors } from '@nestjs/common'
+import { ResponseDTO } from 'src/utils/ReponseDTO'
+import { AttachmentsService } from './AttachmentsService'
+import { FileInterceptor } from '@nestjs/platform-express'
 
 @Controller('attachments')
-export class AttachmentsController {}
+export class AttachmentsController {
+  constructor(private readonly attachmentsService: AttachmentsService) {}
+
+  @Post('upload')
+  @UseInterceptors(FileInterceptor('file'))
+  async uploadFile(@UploadedFile() file: Express.Multer.File, @Body('doctorId') doctorId: string, @Body('patientId') patientId: string) {
+    try {
+      const document = await this.attachmentsService.uploadFile(file, Number(doctorId), Number(patientId))
+      return new ResponseDTO(HttpStatus.CREATED, 'Documento salvo com sucesso.', document)
+    } catch (error: unknown) {
+      if (error instanceof HttpException) throw error
+      if (error instanceof Error) {
+        throw new InternalServerErrorException(error.message, 'Erro inesperado')
+      }
+      throw new InternalServerErrorException('Erro inesperado')
+    }
+  }
+}
--- src/attachments/AttachmentsModules.ts
+++ src/attachments/AttachmentsModules.ts
@@ -1,8 +1,8 @@
-import { Module } from '@nestjs/common';
-import { AttachmentsService } from './AttachmentsService';
-import { AttachmentsController as AttachmentsController } from './AttachmentsController';
-import { TypeOrmModule } from '@nestjs/typeorm';
-import { AttachmentsEntity } from './AttachmentsEntity';
+import { Module } from '@nestjs/common'
+import { AttachmentsService } from './AttachmentsService'
+import { AttachmentsController as AttachmentsController } from './AttachmentsController'
+import { TypeOrmModule } from '@nestjs/typeorm'
+import { AttachmentsEntity } from './AttachmentsEntity'
 
 @Module({
   imports: [TypeOrmModule.forFeature([AttachmentsEntity])],
